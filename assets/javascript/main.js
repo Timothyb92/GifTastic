3162a08d238738f0bad0757bca0a2db3305d6d6f
Timothy Bedsole <timbedsole@gmail.com>
2018-07-28 15:06:49 -0400

    played around with code a bit to try to fix country buttons not loading gifs ater a country is added to the list. No dice.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,10 @@
+// $(".gif").click(function(){
+//     console.log("Clicked gif");
+//     if($(this).attr("data-state") == "still"){
+//         $(this).attr("src", $(this).attr("data-animate"));
+//     }
+// })
 $(document).ready(function(){
-
     //Array that holds buttons that are shown when the page loads
     var buttonList = ["America", "Germany", "South Korea", "Australia", "Great Britain", "China", "Peru", "Brazil", "Panama", "Norway", "Iceland", "Ireland", "India", "Singapore", "Malaysia", "France", "Cambodia", "South Africa", "Sweden", "Italy", "Russia", "Cuba", "Honduras", "Colombia", "Egypt", "Myanmar (Burma)", "The Philippines"];
 
@@ -17,41 +22,46 @@ $(document).ready(function(){
             $("#buttons").append(countryButton);
         })
 
-        //Listens to the Submit button to add a new country button to the list and renders the screen
-        $("#addCountry").click(function(){
-            event.preventDefault();
-            if ($("#countryInput").val() != ""){
-                buttonList.push($("#countryInput").val().trim());
-                renderButtons();
-                //Sets the input field back to empty after the user enters a value
-                $("#countryInput").val("");
-            }
-        })
         
     };
-    renderButtons();
 
+    renderButtons();
+    
+    //Listens to the Submit button to add a new country button to the list and renders the screen
+    $("#addCountry").click(function(){
+        event.preventDefault();
+        renderButtons();
+        console.log("Submit button clicked and renderButtons method ran");
+        if ($("#countryInput").val() != ""){
+            var country = $("#countryInput").val().trim();
+            buttonList.push(country);
+            renderButtons();
+            //Sets the input field back to empty after the user enters a value
+            $("#countryInput").val("");
+        }
+    })
+    
     $(".countryButtons").click(function(){
         //API call that searches the text in the button clicked
         var searchParam = $(this).attr("data-name");
-        console.log(searchParam);
+        console.log($(this).attr("data-name"));
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=417ePfutZTGs4Q4fMPQJhskpEMyUZVfN&q=" + searchParam + "&limit=25&offset=0&rating=PG&lang=en";
-
+        
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(data){
-            console.log(queryURL);
-            console.log(data.data);
             //Creates a div that will hold the gif and information about the gif
             for (var i = 0; i <= 9; i++){
                 var gifDiv = $("<div>");
-                var gif = $("<img>");
-                img.attr("src", data.data[i].images.original_still.url);
-                img.attr("data-state", "still");
+                var gif = $("<img>").addClass("gif");
+                gif.attr("src", data.data[i].images.fixed_height_still.url);
+                gif.attr("data-animate", data.data[i].images.fixed_height.url);
+                gif.attr("data-state", "still");
                 gifDiv.append(gif);
                 $("#gifContainer").prepend(gifDiv);
             }
         })
     })
+    
 })
