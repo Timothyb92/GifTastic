e2c149f863751da884dd3a9e7a957d232323e2d5
Timothy Bedsole <timbedsole@gmail.com>
2018-07-29 16:06:43 -0400

    Added styling to the cards holding the gifs, and fixed issue where once the Add country button was clicked, GIFs would no longer load

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
     renderButtons();
     
     //Listens to the Submit button to add a new country button to the list and renders the screen
-    $("#addCountry").click(function(){
+    $("#addCountry").click(function(event){
         event.preventDefault();
         //Only runs if the field has a value inside of it
         if ($("#countryInput").val() != ""){
@@ -34,52 +34,56 @@ $(document).ready(function(){
         }
     })
     
-    $(".countryButtons").click(function(){
-        //API call that searches the text in the button clicked
-        var searchParam = $(this).attr("data-name");
-        console.log($(this).attr("data-name"));
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=417ePfutZTGs4Q4fMPQJhskpEMyUZVfN&q=" + searchParam + "&limit=25&offset=0&rating=PG&lang=en";
+    // $(".countryButtons").click(function(){
         
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(data){
-            console.log(data);
-            //Empties the part of the page that holds GIFs so when another country button is clicked, it clears the previous section
-            $("#gifContainer").empty();
-            //Creates 9 divs that will hold the gifs and information about the gifs
-            for (var i = 0; i <= 9; i++){
-                var gifDiv = $("<div>").addClass("gifDiv float-left card");
-                var gif = $("<img>").addClass("gif");
-                var rating = $("<p>").addClass("rating card-header text-center");
-                //assigns data-still and data-animate to the src of both the still an animated versions of the gif
-                gif.attr("data-still", data.data[i].images.fixed_height_still.url)
-                gif.attr("data-animate", data.data[i].images.fixed_height.url);
-                //sets the gif to be still once it loads
-                gif.attr("data-state", "still");
-                gif.attr("src", data.data[i].images.fixed_height_still.url);
-                rating.text("Rating: " + data.data[i].rating);
-                gifDiv.append(rating);
-                gifDiv.append(gif);
-                $("#gifContainer").prepend(gifDiv);
-            }
-
-            //When a gif is clicked, it will animate or pause depending on the state it's currently in
-            $(".gif").click(function(){
-                if($(this).attr("data-state") == "still"){
-                    //Sets the src of the image to the animated gif
-                    $(this).attr("src", $(this).attr("data-animate"));
-                    //Sets data-state to animate
-                    $(this).attr("data-state", "animate");
+        function displayCountryGifs (){
+            //API call that searches the text in the button clicked
+            var searchParam = $(this).attr("data-name");
+            console.log($(this).attr("data-name"));
+            var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=417ePfutZTGs4Q4fMPQJhskpEMyUZVfN&q=" + searchParam + "&limit=25&offset=0&rating=PG&lang=en";
+            
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function(data){
+                console.log(data);
+                //Empties the part of the page that holds GIFs so when another country button is clicked, it clears the previous section
+                $("#gifContainer").empty();
+                //Creates 9 divs that will hold the gifs and information about the gifs
+                for (var i = 0; i <= 9; i++){
+                    var gifDiv = $("<div>").addClass("gifDiv float-left card");
+                    var gif = $("<img>").addClass("gif");
+                    var rating = $("<p>").addClass("rating card-header text-center");
+                    //assigns data-still and data-animate to the src of both the still an animated versions of the gif
+                    gif.attr("data-still", data.data[i].images.fixed_height_still.url)
+                    gif.attr("data-animate", data.data[i].images.fixed_height.url);
+                    //sets the gif to be still once it loads
+                    gif.attr("data-state", "still");
+                    gif.attr("src", data.data[i].images.fixed_height_still.url);
+                    rating.text("Rating: " + data.data[i].rating);
+                    gifDiv.append(rating);
+                    gifDiv.append(gif);
+                    $("#gifContainer").prepend(gifDiv);
                 }
-                else if ($(this).attr("data-state") == "animate") {
-                    //Sets the src of the image to the still image
-                    $(this).attr("src", $(this).attr("data-still"));
-                    //Changes data-state to still
-                    $(this).attr("data-state", "still");
-                }
+                
+                //When a gif is clicked, it will animate or pause depending on the state it's currently in
+                $(".gif").click(function(){
+                    if($(this).attr("data-state") == "still"){
+                        //Sets the src of the image to the animated gif
+                        $(this).attr("src", $(this).attr("data-animate"));
+                        //Sets data-state to animate
+                        $(this).attr("data-state", "animate");
+                    }
+                    else if ($(this).attr("data-state") == "animate") {
+                        //Sets the src of the image to the still image
+                        $(this).attr("src", $(this).attr("data-still"));
+                        //Changes data-state to still
+                        $(this).attr("data-state", "still");
+                    }
+                })
             })
-        })
-    })
+            // })
+        }
+        $(document).on("click", ".countryButtons", displayCountryGifs);
     
 })
